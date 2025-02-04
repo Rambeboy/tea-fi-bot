83f24f1ace7bea32615f38ac18630f6f31eb0475
fanstrong <kingindoslayer@gmail.com>
2025-02-04 12:43:06 +0700

    Update config_tmp.js

--- config/config_tmp.js
+++ config/config_tmp.js
@@ -2,4 +2,5 @@ export class Config {
   static INITIALDEPOSITAMOUNT = "0.1"; //POL > WPOL
   static TXAMOUNT = "0.001"; //WPOL > TPOL
   static WRAPPINGCOUNT = 30;
+  static WAITFORBLOCKCONFIRMATION = true;
 }
